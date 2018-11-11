88da6109dcd0af87bf0849714b01b014b81bceec
Jelle Geraets <jellegeraets@hotmail.com>
2018-11-11 10:27:23 +0100

    incomplete push (to transfer progress to another PC to work on)

--- src/main/java/communication/messagegenerator/IMessageGenerator.java
+++ src/main/java/communication/messagegenerator/IMessageGenerator.java
@@ -3,5 +3,7 @@ package communication.messagegenerator;
 import java.util.List;
 
 public interface IMessageGenerator {
-    void sendKill();
+    void concludeOrder(int orderId, String conclusion);
+    void confirmOrder(String operatorId, String unitId, String unitName, int orderId, String reason, boolean accepted);
+    void register(String unitName);
 }
--- src/main/java/communication/messagegenerator/MessageGenerator.java
+++ src/main/java/communication/messagegenerator/MessageGenerator.java
@@ -1,5 +1,8 @@
 package communication.messagegenerator;
 
+import communication.messages.managermessages.MessageConcludeOrder;
+import communication.messages.managermessages.MessageConfirmOrder;
+import communication.messages.managermessages.MessageRegister;
 import communication.websockets.IClientWebsocket;
 
 public class MessageGenerator implements IMessageGenerator {
@@ -10,8 +13,21 @@ public class MessageGenerator implements IMessageGenerator {
         this.socket = socket;
     }
 
-    public void sendKill() {
-        Object msg = new Object();
+    @Override
+    public void concludeOrder(int orderId, String conclusion) {
+        MessageConcludeOrder msg = new MessageConcludeOrder(orderId, conclusion);
+        socket.send(msg);
+    }
+
+    @Override
+    public void confirmOrder(String operatorId, String unitId, String unitName, int orderId, String reason, boolean accepted) {
+        MessageConfirmOrder msg = new MessageConfirmOrder(operatorId, unitId, unitName, orderId, reason, accepted);
+        socket.send(msg);
+    }
+
+    @Override
+    public void register(String unitName) {
+        MessageRegister msg = new MessageRegister(unitName);
         socket.send(msg);
     }
 }
